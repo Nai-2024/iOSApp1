8cd5b22d88db7fd0869386ad717766df02f4cfcb
Din <Din.Salehy@triosstudent.com>
2025-01-23 08:54:48 -0500

    Initial Commit

---
+++ Assignment_1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment_1
+//
+//  Created by Din Salehy on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment_1
+//
+//  Created by Din Salehy on 2025-01-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
